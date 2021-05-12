d4bb9c57292cb0c71e803e8b9c2fa419af8100e0
Madala Venkat <19bd1a050z@gmail.com>
2021-05-12 20:29:42 +0530

    commit

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var button=findViewById<Button>(R.id.button)
+        var text=findViewById<TextView>(R.id.text)
+        var input_result=findViewById<EditText>(R.id.input_radius)
+        var flag=false
+        button.setOnClickListener(View.OnClickListener {
+            var radius:Double=input_result.text.toString().toDouble()
+            val pi:Double=3.141
+            var area = pi * radius * radius
+            text.setText("Result = $area")
+        }
+
+
+        )
+
     }
 }
--- app/src/main/java/com/example/myfirstapplication/main.kt
+++ app/src/main/java/com/example/myfirstapplication/main.kt
@@ -1,5 +1,29 @@
 package com.example.myfirstapplication
 
 fun main(){
-    print("Hello World")
+   /* print("Hello World \t ")
+    var myInt=45L
+
+    var myChar: Char = 's'
+    print("print \n $myChar")
+
+    var name="venky"
+    var fc=name[0]
+    var lc=name[name.length-1]
+    print("\n")
+    print("$fc and $lc")
+
+    var myArray=arrayOf(1,2,"hello",'r',3.4);
+    print("${myArray[4]}")
+    print("${myArray.size}")
+
+    var myString:String="Android Kotlin"
+    var myFloat:Float=13.37f
+    val myDouble:Double=3.14159265368979
+    var myShort=25
+*/
+
+
+
+
 }
